8d4300b259f408e6ed72cb1d48181bd0870339c3
Aleksa <aleksa47@gmail.com>
2017-12-28 20:29:12 +0100

    + Nav initial tests and implementation + future checkbox next to events nav link

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,85 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
 
-class Nav extends Component {
-  render() {
-    return <nav />;
-  }
-}
+const Nav = props => {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-light fixed-top">
+      <div className="container">
+        <Link className="navbar-brand" to="/events">
+          Eventer
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarsExample09"
+          aria-controls="navbarsExample09"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarsExample09">
+          <ul className="navbar-nav mr-auto">
+            <LiNavLink
+              classes="nav-item form-inline"
+              to="/events"
+              activeOnlyWhenExact={true}
+              label="Events"
+            />
+            {props.user && (
+              <LiNavLink
+                classes="nav-item"
+                to="/events/new"
+                activeOnlyWhenExact={true}
+                label="New Event"
+              />
+            )}
+          </ul>
+          <ul className="navbar-nav navbar-right">
+            {props.user && (
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  data-toggle="dropdown"
+                  role="button"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  {props.user}
+                </a>
+                <div className="dropdown-menu" aria-labelledby="dropdown09">
+                  <a className="dropdown-item" href="#">
+                    Log out
+                  </a>
+                </div>
+              </li>
+            )}
+          </ul>
+          {!props.user && (
+            <button id="login-btn" className="btn btn-info" type="button">
+              Log in
+            </button>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+const LiNavLink = ({ label, to, activeOnlyWhenExact, classes }) => (
+  <Route
+    path={to}
+    exact={activeOnlyWhenExact}
+    children={({ match }) => (
+      <li className={classes + (match ? ' active' : '')}>
+        <Link className="nav-link" to={to}>
+          {label}
+        </Link>
+      </li>
+    )}
+  />
+);
 
 export default Nav;
---
+++ src/components/nav/Nav.test.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import enzyme, { mount } from 'enzyme';
+import sinon from 'sinon';
+import Adapter from 'enzyme-adapter-react-16';
+import { MemoryRouter } from 'react-router-dom';
+import Nav from './Nav';
+
+enzyme.configure({ adapter: new Adapter() });
+
+describe('Nav', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    );
+  });
+
+  it('should have an "Eventer" brand link', () => {
+    const brand = wrapper.find('Link.navbar-brand');
+    expect(brand.text()).toEqual('Eventer');
+  });
+
+  it('should have "Events" nav link', () => {
+    const eventLink = wrapper.find('LiNavLink Link').find({ to: '/events' });
+    expect(eventLink.text()).toEqual('Events');
+  });
+
+  it('should show "New Event" nav link if props.user', () => {
+    expect(
+      wrapper.find('LiNavLink').find({ to: '/events/new' }).length
+    ).toEqual(0);
+    wrapper = mount(
+      <MemoryRouter>
+        <Nav user="Aleksa" />
+      </MemoryRouter>
+    );
+    const eventLink = wrapper
+      .find('LiNavLink Link')
+      .find({ to: '/events/new' });
+    expect(eventLink.text()).toEqual('New Event');
+  });
+
+  it('should show "Login" button if no props.user', () => {
+    expect(wrapper.find('button#login-btn').text()).toEqual('Log in');
+    wrapper = mount(
+      <MemoryRouter>
+        <Nav user="Aleksa" />
+      </MemoryRouter>
+    );
+    expect(wrapper.find('button#login-btn').length).toEqual(0);
+  });
+
+  it('should show dropdown menu with "props.user" text if props.user', () => {
+    expect(wrapper.find('a.dropdown-toggle').length).toEqual(0);
+    wrapper = mount(
+      <MemoryRouter>
+        <Nav user="Aleksa" />
+      </MemoryRouter>
+    );
+    expect(wrapper.find('a.dropdown-toggle').text()).toEqual('Aleksa');
+    wrapper = mount(
+      <MemoryRouter>
+        <Nav user="Nada" />
+      </MemoryRouter>
+    );
+    expect(wrapper.find('a.dropdown-toggle').text()).toEqual('Nada');
+    expect(wrapper.find('div.dropdown-menu a.dropdown-item').text()).toEqual(
+      'Log out'
+    );
+  });
+});
